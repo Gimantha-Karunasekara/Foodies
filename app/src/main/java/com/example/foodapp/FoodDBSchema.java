a1b29c1bd9d85720debeb848e6b37477e8a51467
Gimantha-Karunasekara <gimanthakarunasekara@gmail.com>
2022-09-05 14:51:15 +0530

    Deleted test class (temp.java)

--- app/src/main/java/com/example/foodapp/FoodDBSchema.java
+++ app/src/main/java/com/example/foodapp/FoodDBSchema.java
@@ -5,8 +5,7 @@ public class FoodDBSchema {
         public static final String NAME = "restaurants";
         public static class Cols{
             public static final String ID = "id";
-            public static final String NAME = "name"
-                    ;
+            public static final String NAME = "name";
         }
 
     }
